5381f6948bc04c21d8ad74d16754698ae5bc510c
Jhonathas César <jhonathascesar232@gmail.com>
2021-03-18 19:55:48 -0300

    Adicionado settings.jsosn para testes

--- app/tests/test_envio.py
+++ app/tests/test_envio.py
@@ -5,6 +5,19 @@ def test_ehlo():
     '''
     Testa se a conexão foi ok
     '''
+    # criado conexão com gmail
     conexao = smtplib.SMTP('smtp.gmail.com', 587)
     res = conexao.ehlo()
+    assert res[0] in range(200, 299)
+
+
+def test_protocolo_tls():
+    '''
+    Utilizado para seguranca de senhas
+    '''
+    # criado conexão com gmail
+    conexao = smtplib.SMTP('smtp.gmail.com', 587)
+    # res = conexao.ehlo()  # teste da conexao
+    conexao.starttls()  # Criptohgrafa os dados
+    res = conexao.login(user=EMAIL, password=PASSWORD)
     assert res[0] in range(200, 299)
