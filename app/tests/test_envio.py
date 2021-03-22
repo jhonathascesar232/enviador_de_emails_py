b881459585e4673fd4baebb2d407594bb3ad8899
Jhonathas César <jhonathascesar232@gmail.com>
2021-03-22 19:25:17 -0300

    Adiconado os imports email.message e ssl

--- app/tests/test_envio.py
+++ app/tests/test_envio.py
@@ -1,4 +1,7 @@
 import smtplib
+from pathlib import Path
+
+BASE_DIR = Path(__file__).resolve().parent.parent  # DIR app
 
 
 def test_ehlo():
@@ -15,9 +18,16 @@ def test_protocolo_tls():
     '''
     Utilizado para seguranca de senhas
     '''
+    from app.app.main import ler_arquivo
+
+    data = ler_arquivo('settings.json')
     # criado conexão com gmail
     conexao = smtplib.SMTP('smtp.gmail.com', 587)
     # res = conexao.ehlo()  # teste da conexao
     conexao.starttls()  # Criptohgrafa os dados
-    res = conexao.login(user=EMAIL, password=PASSWORD)
+    res = conexao.login(
+        user=data['EMAIL'],
+        password=data['PASSWORD']
+    )
+    print('RES-->', res)
     assert res[0] in range(200, 299)
